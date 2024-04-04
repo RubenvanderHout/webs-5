1fd9a9b5dc064d1e3769f56d854950ec45afff39
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-04-04 12:35:36 +0200

    Compose files

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 import "dotenv/config";
-import { buildserver } from "./server";
-import logger from "./utils/logger";
 import http from "http";
 
-const port = Number(process.env.PORT ?? 3000);
-const host = String(process.env.HOST ?? "0.0.0.0");
+const port = '1000'
+const host = '0.0.0.0'
 
 async function main() {
-  try {
-    const server = await buildserver();
+    const server = express();
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: false }));
     const app = http.createServer(server);
 
     app.listen(port, host, () => {
-      logger.info(`Started server on port ${port}`);
+        logger.info(`Started server on port ${port}`);
     });
 
-  } catch (error) {
-    logger.fatal(error);
-  }
+
 }
 
 
 main();
+
+
+
+
+
+    
+
+  
---
+++ modules/auth/app.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import http from "http";
+
+const port = '5000'
+const host = '0.0.0.0'
+
+async function main() {
+    const server = express();
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: false }));
+    const app = http.createServer(server);
+
+    app.listen(port, host, () => {
+        logger.info(`Started server on port ${port}`);
+    });
+
+
+}
+
+
+main();
+
+
+
+
+
+    
+
+  
