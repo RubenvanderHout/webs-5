38aea42cecee1b185ed69dc8da5648d3d604fdc2
Wesley Vink <w.vink2@student.avans.nl>
2024-04-06 21:02:14 +0200

    removed unnecessary function

--- modules/targets/app.js
+++ modules/targets/app.js
@@ -35,19 +35,16 @@ async function main() {
                 const message = JSON.stringify({
                     filename: req.body.filename,
                     username: req.body.username,
-                    end: req.body.end
+                    start: req.body.start,
+                    end: req.body.end,
+                    competition_id: req.body.competition_id
                 });
-                // Connect to RabbitMQ server
                 const connection = await amqp.connect('amqp://localhost');
-                // Create a channel
                 const channel = await connection.createChannel();
-                // Assert the queue
                 const queueName = 'file_queue';
                 await channel.assertQueue(queueName, { durable: false });
-                // Send message to the queue
                 channel.sendToQueue(queueName, Buffer.from(message));
                 console.log(`Message sent to queue '${queueName}': ${message}`);
-                // Close the channel and connection
                 await channel.close();
                 await connection.close();
             } catch (error) {
@@ -60,18 +57,6 @@ async function main() {
 
         }
     });
-    server.get('/rabbitget', async (req, res) => {
-        try {
-            // Receive messages from RabbitMQ queue
-            receiveMessageFromQueue('file_queue', (message) => {
-                console.log(`Received message: ${message}`);
-                res.json({ message }); // Responding to the client with the received message
-            });
-        } catch (error) {
-            res.status(500).json({ error: 'Internal Server Error' });
-            console.error('Error:', error);
-        }
-    });
         
     server.get('/download', async (req, res) => {
         try {
@@ -80,7 +65,7 @@ async function main() {
             console.log("Downloading picture:", filename);
             const fileData = await downloadPictureFromAzureStorage(accountName, accountKey, containerName, filename);
             console.log("Picture downloaded successfully.");
-            res.setHeader('Content-Type', 'image/jpeg'); // Set the appropriate content type
+            res.setHeader('Content-Type', 'image/jpeg');
             res.send(fileData);
         } catch (error) {
             res.status(500).json({ error: 'Internal Server Error' });
@@ -91,40 +76,12 @@ async function main() {
 
 }
 
-const receiveMessageFromQueue = async (queueName, callback) => {
-    try {
-        // Connect to RabbitMQ server
-        const connection = await amqp.connect('amqp://localhost');
-        // Create a channel
-        const channel = await connection.createChannel();
-        // Assert the queue
-        await channel.assertQueue(queueName, { durable: false });
-        // Consume messages from the queue
-        channel.consume(queueName, async (msg) => {
-            if (msg !== null) {
-                // Execute callback with the received message
-                callback(msg.content.toString());
-                // Acknowledge the message
-                channel.ack(msg);
-            }
-        });
-        console.log(`Waiting for messages from queue '${queueName}'...`);
-    } catch (error) {
-        console.error('Error receiving messages from queue:', error);
-    }
-};
+
 async function uploadPictureToAzureStorage(accountName, accountKey, containerName, blobName, fileData) {
-    // Create BlobServiceClient object
     const blobServiceClient = BlobServiceClient.fromConnectionString(`DefaultEndpointsProtocol=https;AccountName=${accountName};AccountKey=${accountKey};EndpointSuffix=core.windows.net`);
-
-    // Get a reference to a container
     const containerClient = blobServiceClient.getContainerClient(containerName);
-
-    // Get a block blob client
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
     try {
-        // Upload picture data to Azure Storage
         await blockBlobClient.uploadData(fileData);
         console.log("Picture uploaded successfully.");
     } catch (error) {
@@ -136,7 +93,6 @@ async function downloadPictureFromAzureStorage(accountName, accountKey, containe
     const blobServiceClient = BlobServiceClient.fromConnectionString(`DefaultEndpointsProtocol=https;AccountName=${accountName};AccountKey=${accountKey};EndpointSuffix=core.windows.net`);
     const containerClient = blobServiceClient.getContainerClient(containerName);
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-    
     const downloadBlockBlobResponse = await blockBlobClient.download(0);
     const fileData = await streamToBuffer(downloadBlockBlobResponse.readableStreamBody);
     return fileData;
